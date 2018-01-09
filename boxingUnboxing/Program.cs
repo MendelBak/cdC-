62772f6392a0a70de7a181a41db6c82a76c6faee
Mendel Bakaleynik <mendelbak@gmail.com>
2018-01-08 18:32:23 -0600

    Finished boxing/unboxing assignment

---
+++ boxingUnboxing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> testList = new List<object>();
+            testList.Add(7);
+            testList.Add(28);
+            testList.Add(-1);
+            testList.Add(true);
+            testList.Add("Chair");
+
+            foreach(var entry in testList) {
+            Console.WriteLine("Here is one of the values in the testList: " + entry);
+            }
+
+            int sum = 0;
+            foreach(var entry in testList) {
+                if(entry is int) {
+                sum += (int)entry;
+                }
+            }
+            Console.WriteLine("The sum of all the ints in the list is: " + sum);
+        }
+    }
+}

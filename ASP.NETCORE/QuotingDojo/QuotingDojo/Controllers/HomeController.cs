2777bd5fc71ad1bee7102454188babcae31bdeaa
Mendel Bakaleynik <mendelbak@gmail.com>
2018-01-17 13:17:24 -0600

    Finshed Quoting Dojo Assingment

--- ASP.NETCORE/QuotingDojo/QuotingDojo/Controllers/HomeController.cs
+++ ASP.NETCORE/QuotingDojo/QuotingDojo/Controllers/HomeController.cs
@@ -3,17 +3,42 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using Newtonsoft.Json;
+using DbConnection;
 
 namespace QuotingDojo.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /Home/
         [HttpGet]
         [Route("")]
-        public IActionResult Index()
+        public IActionResult Index() 
         {
-            return View();
+            List<Dictionary<string, object>> AllUsers = DbConnector.Query("SELECT * FROM quotes_data");
+            ViewBag.AllUsers = AllUsers;
+            return View("index");
         }
+
+        [HttpPost]
+        [Route("submitQuote")]
+        public IActionResult submitQuote(string name, string quote) {
+            string Name = '"' + name + '"';
+            string Quote = '"' + quote + '"';
+            string NowTime = "NOW()";
+             
+
+            DbConnector.Execute($"INSERT INTO quotes_data (name, quote, created_at) VALUES ({Name}, {Quote}, {NowTime})");
+            return RedirectToAction("Results");
+        }
+
+        [HttpGet]
+        [Route("Results")]
+        public IActionResult Results() 
+        {
+            ViewBag.quotes_data = DbConnector.Query($"SELECT * FROM quotes_data");
+            return View("results");
+        }
+
     }
 }

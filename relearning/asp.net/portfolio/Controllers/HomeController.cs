124105f46936ac133e86a2a1c08463557a58212c
Mendel Bak <mendelbak@gmail.com>
2018-05-04 12:08:08 -0500

    Finished portfolio assignment. It's really poorly done, but technically done. I'm ready for the weekend....

---
+++ relearning/asp.net/portfolio/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Projects")]
+        public IActionResult Projects()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            System.Console.WriteLine();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("contactSubmit")]
+        public IActionResult contactSubmit(string name, string email, string message)
+        {
+            System.Console.WriteLine(name + message + email);
+            return Redirect("Contact");
+        }
+
+    }
+}

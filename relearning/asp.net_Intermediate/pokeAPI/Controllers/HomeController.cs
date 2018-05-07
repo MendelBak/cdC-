d98173a3ffccca5d537845dc81a1ddd8c28596f0
Mendel Bak <mendelbak@gmail.com>
2018-05-07 17:06:16 -0500

    Poke API caller makes calls to API. Needs work but it works on an MVP level.

---
+++ relearning/asp.net_Intermediate/pokeAPI/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using quotingDojo.Models;
+using System.Net.Http;
+
+namespace quotingDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("{pokeId}")]
+        public IActionResult Index(int pokeId)
+        {
+            var PokeInfo = new Dictionary<string, object>();
+            WebRequest.GetPokemonDataAsync(pokeId, ApiResponse =>
+                {
+                    PokeInfo = ApiResponse;
+                }
+            ).Wait();
+            ViewBag.PokeInfo = PokeInfo;
+            return View();
+        }
+    }
+}

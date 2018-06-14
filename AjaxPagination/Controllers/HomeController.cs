d16ae4cc7949a271f116ae3b9249cf6de9fce7ae
Mendel Bak <mendelbak@gmail.com>
2018-06-13 23:01:11 -0500

    Added method, working on the search query. Page numbers show up and route pretty well. Still needs some work.

--- AjaxPagination/Controllers/HomeController.cs
+++ AjaxPagination/Controllers/HomeController.cs
@@ -22,11 +22,37 @@ namespace AjaxPagination.Controllers
 
         [HttpGet]
         [Route("")]
-        public IActionResult Index()
+        public IActionResult Index(string FirstName)
         {
-            var testString = _context.Users.ToList();
-            ViewBag.testString = testString;
+            // Do a math.ceiling
+            int TotalUsers = (_context.Users.Count() / 3);
+            List<User> AllUsers = _context.Users.ToList();
+            ViewBag.TotalUsers = TotalUsers;
+            ViewBag.AllUsers = AllUsers;
             return View();
         }
+
+        [HttpPost]
+        [Route("FilterUsers")]
+        public IActionResult FilterUsers(string LastName)
+        {
+            // Do a math.ceiling
+            int TotalUsers = (_context.Users.Count() / 3);
+            List<User> AllUsers = _context.Users.Where(e => e.last_name == LastName).ToList();
+            ViewBag.TotalUsers = TotalUsers;
+            ViewBag.AllUsers = AllUsers;
+            return View("Index");
+        }
+
+        // This route takes the id of the last user that is being displayed on the view as well as the page (via pagination of all the users) that the user wants to go to and returns the approriate reponse.
+        [HttpGet]
+        [Route("ChoosePage/{lastId}/{selection}")]
+        public IActionResult ChoosePage(int lastId, int selection)
+        {
+            System.Console.WriteLine("FOUND THE CORRECT SELECTION! HERE IT IS ->" + lastId + selection);
+            return RedirectToAction("Index");
+        }
+
+        
     }
 }
